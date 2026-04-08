82d46d88421e72517b684aeeba0161e8f4db1d92
Artur Kutsenko <a.kutsenko06@gmail.com>
2026-04-08 13:50:46 +0300

    Add: count_sentences method

--- analyzer.py
+++ analyzer.py
@@ -4,3 +4,7 @@ class TextAnalyzer:
     def read_file(self, filepath):
         with open(filepath, 'r', encoding='utf-8') as f:
             return f.read()
+    def count_sentences(self, text):
+        # Речення закінчуються на: ..., ., !, ?
+        sentences = re.findall(r'\.{3}|[.!?]', text)
+        return len(sentences)
